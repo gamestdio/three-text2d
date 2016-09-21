17e0a55031bcd369d97bad36978c6c8d8abbf287
Endel Dreyer <endel.dreyer@gmail.com>
2016-09-21 20:49:47 +0200

    add 'npm run dist' task to generate distribution files. closes #10

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
-import { Vector2 } from "three";
+import THREE = require("three");
 
 export const textAlign = {
-  center: new Vector2(0, 0),
-  left: new Vector2(1, 0),
-  topLeft: new Vector2(1, -1),
-  topRight: new Vector2(-1, -1),
-  right: new Vector2(-1, 0),
-  bottomLeft: new Vector2(1, 1),
-  bottomRight: new Vector2(-1, 1),
+  center: new THREE.Vector2(0, 0),
+  left: new THREE.Vector2(1, 0),
+  topLeft: new THREE.Vector2(1, -1),
+  topRight: new THREE.Vector2(-1, -1),
+  right: new THREE.Vector2(-1, 0),
+  bottomLeft: new THREE.Vector2(1, 1),
+  bottomRight: new THREE.Vector2(-1, 1),
 }
 
 
