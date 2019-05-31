191dcf06ea39bc9f139df64ed642fc82a2ec25d1
Nikola Pantic <nphemma@yahoo.se>
2019-06-01 00:00:34 +0200

    Add bottom and top text aligns
    Solves #35 by adding new alignments.

--- src/utils.ts
+++ src/utils.ts
@@ -3,9 +3,11 @@ import { Vector2 } from "three";
 export const textAlign = {
   center: new Vector2(0, 0),
   left: new Vector2(1, 0),
+  top: new Vector2(0, -1),
   topLeft: new Vector2(1, -1),
   topRight: new Vector2(-1, -1),
   right: new Vector2(-1, 0),
+  bottom: new Vector2(0, 1),
   bottomLeft: new Vector2(1, 1),
   bottomRight: new Vector2(-1, 1),
 }
